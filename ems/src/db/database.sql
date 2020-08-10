e6eb065fc4de46aa29d2c32cdceb4ee09187c174
NiMaLiC <nimaliwijesingha123@gmail.com>
2020-08-10 12:36:36 +0530

    adding sql file

---
+++ ems/src/db/database.sql
@@ -0,0 +1,12 @@
+create database test;
+use test;
+create table employee(
+id int(11),
+email varchar(100),
+pass Binary(64),
+type varchar(20),
+name varchar(50),
+age int(3),
+primary key(id)
+);
+
